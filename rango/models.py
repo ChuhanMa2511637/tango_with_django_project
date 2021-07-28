d467694c64b62a4f533f823ed6b54925355e2496
ChuhanMa <2511637m@student.gla.ac.uk>
2021-07-28 17:06:21 +0100

    update

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+ 
     def __str__(self):
         return self.name
 class Page(models.Model):
@@ -9,5 +10,5 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
